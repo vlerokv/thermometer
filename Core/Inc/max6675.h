44203d0434a3ade293ec6302cd720803a0dd456e
vlerokv <mischuklera18@gmail.com>
2025-09-08 23:27:14 +0300

    fix PR comments

--- Core/Inc/max6675.h
+++ Core/Inc/max6675.h
@@ -5,15 +5,14 @@
  *      Author: misch
  */
 
-
-
 #ifndef INC_MAX6675_H_
 #define INC_MAX6675_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "stm32g031xx.h"
 
-uint8_t max6675_check_thermocouple(uint16_t raw_data);
+bool max6675_is_thermocouple_connected(uint16_t raw_data);
 
 uint16_t max6675_read_raw_data(SPI_TypeDef *spi_type, GPIO_TypeDef *gpio_type_cs, uint32_t cs_pin_mask);
 
--- Core/Src/max6675.c
+++ Core/Src/max6675.c
@@ -1,4 +1,4 @@
- /*
+/*
  * max6675.c
  *
  *  Created on: Jul 24, 2025
@@ -8,53 +8,32 @@
 #include "max6675.h"
 #include "stm32g0xx_ll_gpio.h"
 #include "stm32g0xx_ll_spi.h"
+#include "stm32g0xx_ll_bus.h"
 
-uint16_t max6675_read_raw_data(SPI_TypeDef *spi_type, GPIO_TypeDef *gpio_type_cs, uint32_t cs_pin_mask)
-{
-	uint8_t first_byte;
-	uint8_t second_byte;
-	uint16_t raw_data;
+uint16_t max6675_read_raw_data(SPI_TypeDef *spi_type, GPIO_TypeDef *gpio_type_cs, uint32_t cs_pin_mask) {
+    //LL_APB2_GRP1_ReleaseReset(LL_APB2_GRP1_PERIPH_SPI1);
+    LL_GPIO_ResetOutputPin(gpio_type_cs, cs_pin_mask);
+    LL_SPI_Enable(spi_type);
 
-	LL_GPIO_ResetOutputPin(gpio_type_cs, cs_pin_mask);
-	 LL_SPI_Enable(SPI1);
+    // wait until there is smth to read
+    while (!LL_SPI_IsActiveFlag_RXNE(spi_type));
+    uint8_t first_byte = LL_SPI_ReceiveData8(spi_type);
 
+    while (!LL_SPI_IsActiveFlag_RXNE(spi_type));
+    uint8_t second_byte = LL_SPI_ReceiveData8(spi_type);
 
+    LL_SPI_Disable(spi_type);
+    LL_GPIO_SetOutputPin(gpio_type_cs, cs_pin_mask);
+    //LL_APB2_GRP1_ForceReset(LL_APB2_GRP1_PERIPH_SPI1);
 
-	// wait until there is smth to read
-	while(!LL_SPI_IsActiveFlag_RXNE(spi_type));
-	first_byte = LL_SPI_ReceiveData8(spi_type);
-
-
-	while(!LL_SPI_IsActiveFlag_RXNE(spi_type));
-	second_byte = LL_SPI_ReceiveData8(spi_type);
-
-	LL_SPI_Disable(spi_type);
-
-	LL_GPIO_SetOutputPin(gpio_type_cs, cs_pin_mask);
-
-	raw_data = (first_byte << 8) | second_byte;
-
-	return raw_data;
+    return (first_byte << 8) | second_byte;
 }
 
-uint8_t max6675_check_thermocouple(uint16_t raw_data)
-{
-	uint8_t d2;
-
-	d2 = (raw_data >> 2) & 0x01;
-
-	return d2;
+bool max6675_is_thermocouple_connected(uint16_t raw_data) {
+    return !(raw_data & (1 << 2));
 }
 
-float max6675_convert_to_temperature(uint16_t raw_data)
-{
-	uint16_t raw_temp;
-	float temperature_celsius;
-	raw_temp = (raw_data >> 3) & 0x0FFF;
-
-	temperature_celsius = raw_temp * 0.25;
-
-	return temperature_celsius;
-
+float max6675_convert_to_temperature(uint16_t raw_data) {
+    return ((raw_data >> 3) & 0x0FFF) * 0.25;
 }
 
